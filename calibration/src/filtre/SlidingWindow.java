f62219096440fa141d04d91ba2d34062dbbf5a43
Alinoé <Alino@PC-de-Alinoe>
2013-05-23 20:29:07 +0200

    test pour saas

--- calibration/src/filtre/SlidingWindow.java
+++ calibration/src/filtre/SlidingWindow.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class SlidingWindow<E> extends LinkedList<E> {
 
 	private int size;
-	
+	private int test;
 	/**
 	 * Added by default to allow serializable 
 	 */
