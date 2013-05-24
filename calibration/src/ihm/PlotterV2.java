4ea3c27e9ad19d390cd30a15826ca4fa73e18a57
Alinoé <Alino@PC-de-Alinoe>
2013-05-24 18:07:50 +0200

    Nouvelle classe pour essayer le canvas

---
+++ calibration/src/ihm/PlotterV2.java
@@ -0,0 +1,55 @@
+package ihm;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.events.PaintListener;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+
+
+
+
+public class PlotterV2 {
+	private Display display;
+	private Shell shell;
+	private Canvas canvas;
+	private Point point;
+	
+	
+	public Canvas getCanvas(){
+		return this.canvas;
+	}
+	
+	public PlotterV2(){
+		display=new Display();
+		shell=new Shell(display,SWT.None);
+		shell.setText("Canvas Example");
+	    shell.setLayout(new FillLayout());
+	    
+	    Canvas canvas = new Canvas(shell, SWT.NONE);
+
+	    canvas.addPaintListener(new PaintListener() {
+	      public void paintControl(PaintEvent e) {
+	        e.gc.drawPoint(point.x,point.y);
+	      }
+	    });
+	}
+	
+	public void setPoint(int x, int y){
+		point.x=x;
+		point.y=y;
+	}
+	public void execute(){
+		shell.open();
+	    while (!shell.isDisposed()) {
+	      if (!display.readAndDispatch()) {
+	        display.sleep();
+	      }
+	    }
+	    display.dispose();
+	  }
+	}
+	
+}
