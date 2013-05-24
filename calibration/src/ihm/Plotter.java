db1f3f1b8047072915ddcd654cda1be85d52cb51
Alinoé <Alino@PC-de-Alinoe>
2013-05-24 17:14:08 +0200

    test 6

--- calibration/src/ihm/Plotter.java
+++ calibration/src/ihm/Plotter.java
@@ -1,61 +1,35 @@
 package ihm;
 
-import org.eclipse.swt.widgets.*;
-import org.eclipse.swt.*;
-import org.eclipse.swt.graphics.*;
-import org.eclipse.swt.layout.*;
-import swing2swt.layout.BoxLayout;
-import org.eclipse.swt.custom.SashForm;
-import org.eclipse.wb.swt.SWTResourceManager;
-import swing2swt.layout.FlowLayout;
-import swing2swt.layout.BorderLayout;
-import org.eclipse.swt.custom.CLabel;
-import org.eclipse.swt.events.SelectionAdapter;
-import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.dnd.DropTarget;
-import org.eclipse.swt.dnd.DND;
-import org.eclipse.swt.events.KeyAdapter;
-import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.GC;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 
 public class Plotter {
-	private Display display;
-	private Shell fenetre;
-	private Canvas canvas;
-	GC gc1;
 
-	public Plotter(){
-		
-	display= new Display();
-	fenetre = new Shell(display, SWT.SHELL_TRIM);
-	fenetre.setSize(240,240);
-	fenetre.setText("Pour Canvas");
-	fenetre.setLayout(new BorderLayout(0, 0));
-	
-	canvas = new Canvas(fenetre, SWT.NONE); 
-	canvas.setSize(200,200);
-	canvas.setLayoutData(BorderLayout.CENTER);
-	fenetre.pack();
-	}
-	
-	public void drawPoint(int x, int y){
-		gc1.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
-		gc1.drawOval(x,y,20,20);
-		gc1.dispose();
-	}
-	
-	public void execute() {
-		fenetre.open();
-		gc1 = new GC(canvas);
-		gc1.setForeground(display.getSystemColor(SWT.COLOR_BLUE));
-		gc1.drawOval(100,100,20,20);
-		gc1.dispose();
-		
-		while (!fenetre.isDisposed()) {
-			if (!display.readAndDispatch()){
+	public static void execute() {
+		final Display display = new Display();
+		final Shell shell = new Shell(display);
+		shell.setSize(420, 420);
+
+		Canvas canvas = new Canvas(shell, SWT.NONE);
+		canvas.setSize(200, 200);
+		canvas.setLocation(10, 10);
+		shell.pack();
+		shell.open();
+
+		GC gc = new GC(canvas);
+		gc.drawText("Bonjour", 20, 20);
+		gc.drawLine(10, 10, 10, 100);
+		gc.setForeground(display.getSystemColor(SWT.COLOR_RED));
+		gc.drawOval(60, 60, 60, 60);
+		gc.dispose();
+
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch())
 				display.sleep();
-			}
 		}
 		display.dispose();
 	}
-	
 }
