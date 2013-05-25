7acf1674e764729bf4cee65cdcbee187ab62c4c2
florent <florent@debian>
2013-05-25 13:35:07 +0200

    maj Data,Vecteur,TestIMu : intégration du filtre a data

--- calibration/src/data/Data.java
+++ calibration/src/data/Data.java
@@ -3,21 +3,24 @@ package data;
 import java.util.*;
 
 import common.TypeCalibration;
+import filtre.Filter;
 
 public class Data {
 
 	private TypeCalibration calib;
+	private Filter<Vecteur> filtre;
 	List<Vecteur> list = new LinkedList<Vecteur>();
 	
-	public Data(TypeCalibration calibration) {
+	public Data(TypeCalibration calibration,Filter<Vecteur> filtre) {
 		this.calib = calibration;
+		this.filtre = filtre;
 	}
 
 	public void store(double x_raw, double y_raw, double z_raw) {
-
 		Vecteur vec = new Vecteur(x_raw, y_raw, z_raw);
 		vec.setType(calib);
 		list.add(vec);
+		filtre.add(vec);
 	}
 
 	public String toString() {
