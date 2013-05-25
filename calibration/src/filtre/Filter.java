1f3921193818b80a07ac65f7ab440ddf9987e770
florent <florent@debian>
2013-05-25 17:15:21 +0200

    maj filter

--- calibration/src/filtre/Filter.java
+++ calibration/src/filtre/Filter.java
@@ -5,13 +5,14 @@ import java.util.*;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 public class Filter {
 
-	private Collection<DescriptiveStatistics> variables = null;
+	private ArrayList<DescriptiveStatistics> variables;
 	private int windowSize;
 	private SlidingWindow<VecteurFiltrable<Double>> window; 
 	
 	public Filter(int windowSize){
 		this.windowSize=windowSize;
 		window = new SlidingWindow<VecteurFiltrable<Double>>(windowSize);
+		variables = null;
 	}
 	
 	public Filter() {
@@ -28,17 +29,21 @@ public class Filter {
 		Collection<Double> toAdd = v.setArray();
 		Iterator<Double> a = toAdd.iterator();
 		if (variables != null) {
+			System.out.println("variables non null");
 			Iterator<DescriptiveStatistics> d = variables.iterator();
-			while(a.hasNext()){
+			while(a.hasNext() && d.hasNext()){
 				d.next().addValue(a.next());
+				System.out.println("ajoute les donnees");
 			}
 			window.add(v);
 			update();
 		}
 		else {
+			System.out.println("cree variabless");
 			this.variables = new ArrayList<DescriptiveStatistics>(toAdd.size());
-			for(DescriptiveStatistics e : variables){
-				e = new DescriptiveStatistics(windowSize);
+			for(int i=0;i<toAdd.size();i++){
+				System.out.println("cree variable");
+				variables.add(new DescriptiveStatistics(windowSize));
 			}
 			this.add(v);
 		}
