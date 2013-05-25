056885f970cf8e5a21bfba6140bd022c82ce007f
Alinoé <Alino@PC-de-Alinoe>
2013-05-25 17:30:31 +0200

    filter fonctionnel et operationnel

--- calibration/src/filtre/Filter.java
+++ calibration/src/filtre/Filter.java
@@ -1,54 +1,54 @@
 package filtre;
 
-import java.lang.reflect.Array;
 import java.util.*;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
+
 public class Filter {
 
 	private ArrayList<DescriptiveStatistics> variables;
 	private int windowSize;
-	private SlidingWindow<VecteurFiltrable<Double>> window; 
-	
-	public Filter(int windowSize){
-		this.windowSize=windowSize;
+	private SlidingWindow<VecteurFiltrable<Double>> window;
+	private final int noiseThreshold = 3;
+
+	public Filter(int windowSize) {
+		this.windowSize = windowSize;
 		window = new SlidingWindow<VecteurFiltrable<Double>>(windowSize);
 		variables = null;
 	}
-	
+
 	public Filter() {
 		this.windowSize = 40;
 	}
 
-	private void update(){
-		for(DescriptiveStatistics e : variables){
-			System.out.println("std" + e.getStandardDeviation());
+	private void update(VecteurFiltrable<Double> v) {
+		boolean valable = true;
+		for (DescriptiveStatistics e : variables) {
+			valable = (e.getStandardDeviation() < 3) && valable;
 		}
+		if (valable) v.setTrue(); else v.setFalse();   
 	}
-	
-	public void add(VecteurFiltrable<Double> v){
+
+	public void add(VecteurFiltrable<Double> v) {
 		Collection<Double> toAdd = v.setArray();
 		Iterator<Double> a = toAdd.iterator();
 		if (variables != null) {
 			System.out.println("variables non null");
 			Iterator<DescriptiveStatistics> d = variables.iterator();
-			while(a.hasNext() && d.hasNext()){
+			while (a.hasNext() && d.hasNext()) {
 				d.next().addValue(a.next());
 				System.out.println("ajoute les donnees");
 			}
 			window.add(v);
-			update();
-		}
-		else {
+			update(v);
+		} else {
 			System.out.println("cree variabless");
 			this.variables = new ArrayList<DescriptiveStatistics>(toAdd.size());
-			for(int i=0;i<toAdd.size();i++){
+			for (int i = 0; i < toAdd.size(); i++) {
 				System.out.println("cree variable");
 				variables.add(new DescriptiveStatistics(windowSize));
 			}
 			this.add(v);
 		}
 	}
-	
-}
 
-	
+}
