9f99e0b06a27939086d96622011d1fbe54c16619
Alinoé <Alino@PC-de-Alinoe>
2013-05-29 17:30:36 +0200

    ExtractRawData fonctionnel

--- calibration/src/ihm/ExtractRawData.java
+++ calibration/src/ihm/ExtractRawData.java
@@ -1,6 +1,7 @@
 package ihm;
 
 import java.io.File;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Iterator;
 import org.jdom2.*;
@@ -11,7 +12,11 @@ public class ExtractRawData {
 	private org.jdom2.Document document;
 	private Element racine;
 
-	public void parse(String toParse) {
+	/**Used to create the tree of the xml document given in input
+	 * 
+	 * @param toParse
+	 */
+	private void parse(String toParse) {
 		// On crée une instance de SAXBuilder
 		SAXBuilder sxb = new SAXBuilder();
 		try {
@@ -28,43 +33,70 @@ public class ExtractRawData {
 		racine = document.getRootElement();
 	}
 	
-	public String infoNoeud(String nomNoeud){
+	/**test function returning the root of the xml document stored in the object
+	 * 
+	 */
+	public void test(){
+		System.out.println(racine);
+	}
+	
+	/**used to create the information about the DL_SETTING node named telemetry
+	 * 
+	 * @return the var field of the node
+	 */
+	private String infoNoeud(){
 	//On saute systematiquement le premier noeud du fichier
 		List<Element> listdl_settings = racine.getChildren("dl_settings");
 		listdl_settings = listdl_settings.get(0).getChildren("dl_settings");
 		String buf = null;
-		int dlSettingsId1 = 0;
-		int dlSettingsId2 = 0;
 		Iterator<Element> i = listdl_settings.iterator();
-		boolean found = false;
-		do {
-			Iterator<Element> j = (i.next().getChildren("dl_settings")).iterator();
-			dlSettingsId1++;
+		String test = null;
+		Element elem = null;
+		try {
 			do {
-					found = found || (j.next().getAttributeValue("var").toString().equals("telemetry_mode_Main"));
-					dlSettingsId2++;
-			} while (!found && j.hasNext());
-		} while (i.hasNext() && !found);
-		//On redescend pour tenir le bon noeud qui a déjà été mangé.
-		i = listdl_settings.iterator();
-		for(int k=0;k<dlSettingsId1;k++){
-			i.next();
+				elem = i.next();
+				test = elem.getAttribute("var").getName();
+			} while(i.hasNext() && !test.equals("telemetry_mode_Main"));
+		} catch (Exception e){
+			System.out.println("Fichier probablement vide");
+			e.printStackTrace();
 		}
-		i = i.next().getChildren().iterator();
-		for(int k=0;k<dlSettingsId2;k++){
-			i.next();
-		}
-		buf = i.next().getAttributeValue("values").toString();
+		Iterator<Element> j = i.next().getChildren().iterator();
+		buf = j.next().getAttributeValue("values").toString();
 		return buf;
 		}
 		
 		//on tient donc i au noeud dlSettings contenant les modes de telemetry
 		//on recupere la chaine qui va bien
-		
-
+	
+	/**used to transform the node in a list of choice
+	 * 
+	 * @param toParse
+	 * @return list of the possible modes
+	 */
+	private static List<String> parseChoice(String toParse){
+		List<String> res = new LinkedList<String>();
+		String[] temp = toParse.split("\\|");
+		for(int i = 0; i<temp.length; i++){
+			res.add(temp[i]);
+		}
+		return res;
+	}
+	
+	/**main fonction return the possible telemetry modes
+	 * 
+	 * @param toParse
+	 * @return the list of the telemetry modes
+	 */
+	public List<String> extract(String toParse) {
+		this.parse(toParse);
+		return parseChoice(this.infoNoeud());
+	}
+	
 	public static void main(String args[]){
 		ExtractRawData d = new ExtractRawData();
-		d.parse("C:\\Users\\Alinoé\\Desktop");
-		System.out.println(d.infoNoeud(""));
+		d.parse("C:\\Users\\Alinoé\\Desktop\\settings_booz2.xml");
+		d.test();
+		System.out.println(parseChoice(d.infoNoeud()));
 	}
 }
