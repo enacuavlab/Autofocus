33b331cf1210f1e7e2968b933f996927a27456f1
florent <florent@debian>
2013-05-29 18:48:13 +0200

    creation de l'ellipsoide

--- calibration/src/ellipsoide/Ellipsoide.java
+++ calibration/src/ellipsoide/Ellipsoide.java
@@ -1,22 +1,37 @@
 package ellipsoide;
-import java.util.Iterator;
-import java.util.List;
 
-import Jama.Matrix;
+import org.ejml.simple.SimpleMatrix;
+
 import data.*;
-public class Ellipsoide {
-	private Data data;
-	private Matrix coefficientEllipsoide;
-	Ellipsoide(Data data){
-		this.data=data;
-	}
-	public void ajouter(){
-		Matrix A=
-		List<Vecteur> list = data.getVecteur();
-		Iterator i = list.iterator();
-		while(i.hasNext()){
-			
-		}
 
+public class Ellipsoide {
+	private SimpleMatrix coefficientEllipsoide;
+	private SimpleMatrix A;
+	private SimpleMatrix B;
+
+	Ellipsoide(Data data) {
+		B = new SimpleMatrix(0, 1);
+		A = new SimpleMatrix(0, 9);
+		coefficientEllipsoide = new SimpleMatrix(1, 9);
+	}
+
+	public void ajouter(Vecteur v) {
+		double[][] ctable = { {
+				(Math.pow(v.getX(), 2) + Math.pow(v.getY(), 2) - 2 * Math.pow(
+						v.getZ(), 2)),
+				(Math.pow(v.getX(), 2) - 2 * Math.pow(v.getY(), 2) + Math.pow(
+						v.getZ(), 2)), 4 * v.getX() * v.getY(),
+				2 * v.getX() * v.getZ(), 2 * v.getY() * v.getZ(), v.getX(),
+				v.getY(), v.getZ(), 1 } };
+		SimpleMatrix C = new SimpleMatrix(ctable);
+		A.combine(A.numRows(), 0, C);
+		double [][] dtable = { {Math.pow(v.getX(), 2) + Math.pow(v.getY(), 2) + Math.pow(v.getZ(), 2)} };
+		SimpleMatrix D= new SimpleMatrix(dtable);
+		B.combine(B.numRows(),0,D );
+		coefficientEllipsoide=A.solve(B);
+
+	}
+	public void print(){
+		coefficientEllipsoide.print();
 	}
 }
---
+++ calibration/src/ellipsoide/TestEllipsoide.java
@@ -0,0 +1,14 @@
+package ellipsoide;
+
+import Jama.Matrix;
+
+public class TestEllipsoide {
+	public static void main(String args[]) {
+		double[][] vals = { { 1., 2., 3 }, { 4., 5., 6. }, { 7., 8., 10. } };
+		Matrix A = new Matrix(vals);
+		Matrix b = Matrix.random(3, 1);
+		Matrix x = A.solve(b);
+		Matrix r = A.times(x).minus(b);
+		//double rnorm = r.normInf();
+	}
+}
