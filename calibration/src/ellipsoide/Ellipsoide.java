42403e3a33a9b274dc15c48320df6ea4dc33c067
florent <florent@debian>
2013-05-30 10:36:27 +0200

    modification ellipsoide

--- calibration/src/ellipsoide/Ellipsoide.java
+++ calibration/src/ellipsoide/Ellipsoide.java
@@ -24,17 +24,23 @@ public class Ellipsoide {
 				2 * b2.getX() * b2.getZ(), 2 * b2.getY() * b2.getZ(), b2.getX(),
 				b2.getY(), b2.getZ(), 1 } };
 		SimpleMatrix C = new SimpleMatrix(ctable);
-		A = A.combine(A.numRows()+1, 1, C);
+		A = A.combine(SimpleMatrix.END, 0, C);
 		double [][] dtable = { {Math.pow(b2.getX(), 2) + Math.pow(b2.getY(), 2) + Math.pow(b2.getZ(), 2)} };
 		SimpleMatrix D= new SimpleMatrix(dtable);
-		B.combine(B.numRows()+1,1,D );
-		if(A.numRows()>5){
+		B=B.combine(SimpleMatrix.END,0,D );
+		try {
+		if(A.numRows()>9){
 			coefficientEllipsoide=A.solve(B);
+			}
+		} catch (Exception e){
+			System.out.println("pas encore assez de valeurs");
 		}
 
 	}
 	public void print(){
-		A.printDimensions();
-		coefficientEllipsoide.print();
+		//A.printDimensions();
+		//B.printDimensions();
+		//A.print();
+		coefficientEllipsoide.transpose().print();
 	}
 }
