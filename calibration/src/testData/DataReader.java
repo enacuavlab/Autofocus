6541f6fc171014f5c1c81488e683da26020abc4f
Alinoé <Alino@PC-de-Alinoe>
2013-05-31 12:04:53 +0200

    DataReader pour ecalibrer depuis un data

--- calibration/src/testData/DataReader.java
+++ calibration/src/testData/DataReader.java
@@ -1,5 +1,34 @@
 package testData;
 
-public class DataReader {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
 
+public class DataReader implements Iterable {
+	
+	ArrayList<String> lignes = new ArrayList<String>();
+	
+	public DataReader(String arg){
+		// lecture du fichier texte
+		try {
+			InputStream ips = new FileInputStream(arg);
+			InputStreamReader ipsr = new InputStreamReader(ips);
+			BufferedReader br = new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne = br.readLine()) != null) {
+				lignes.add(ligne);
+			}
+			br.close();
+		} catch (Exception e) {
+			System.out.println(e.toString());
+		}
+	}
+
+	@Override
+	public Iterator<String> iterator() {
+		return lignes.iterator();
+	}
 }
