1f0ce51ace2d8d0cfc81edb7a4eca4ca45615ceb
Alinoé <Alino@PC-de-Alinoe>
2013-06-01 18:28:36 +0200

    Classes manquantes car non envoyées au dernier commit

---
+++ calibration/src/filtre/FilterPlot.java
@@ -0,0 +1,36 @@
+package filtre;
+
+import common.TypeCalibration;
+import ihm.GUIHelper;
+import ihm.Plotter;
+
+public class FilterPlot extends Filter{
+
+	private Plotter plot;
+	
+	public FilterPlot(int windowSize, TypeCalibration type) {
+		super(windowSize,type);
+		plot = new Plotter();
+	}
+	
+	public FilterPlot(Plotter plot) {
+		this(10,TypeCalibration.MAGNETOMETER);
+		this.plot = plot;
+	}
+	
+	@Override
+	public void add(VecteurFiltrable<Double> v) {
+		super.add(v);
+		plot.add(v);
+	}
+	
+	public static void main(String args[]) {
+		Plotter plot = new Plotter();
+		FilterPlot f = new FilterPlot(plot);
+		EmulData e = new EmulData(f);
+		Test t = new Test(e);
+		t.run();
+		GUIHelper.showOnFrame(plot, "test");
+	}
+	
+}
---
+++ calibration/src/rawmode/IncorrectXmlException.java
@@ -0,0 +1,14 @@
+package rawmode;
+
+public class IncorrectXmlException extends Exception {
+
+	public IncorrectXmlException(String s, Throwable e) {
+		super(s,e);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
