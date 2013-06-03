f07c7da3a61a957eb230d1fe4d79752e990ceca6
Alinoé <Alino@PC-de-Alinoe>
2013-06-03 22:15:05 +0200

    IvyConfListener V2

--- calibration/src/rawmode/IvyConfigListener.java
+++ calibration/src/rawmode/IvyConfigListener.java
@@ -9,22 +9,29 @@ public class IvyConfigListener {
 
 	
 	private Ivy bus;
+	private int id;
 	
 	public IvyConfigListener(final int idDrone) throws IvyException {
+		id = idDrone;
 		bus = new Ivy("IvyRawListener", "IvyConfigListener Ready", null);
-		bus.bindMsg("(.*)",
+		bus.bindMsg("(.*) CONFIG (.*)",
 				new IvyMessageListener() {
 					public void receive(IvyClient arg0, String[] args) {
-						System.out.println(args[0]);
+						System.out.println(args[0] + " CONFIG " + args[1]);
 					}
 				});
 		// bus.bindMsg("(.*)", this);
 		bus.start(null);
 	}
 	
+	public void sendRequest() throws IvyException {
+		bus.sendMsg("calibrate CONFIG_REQ " + id);
+	}
+	
 	public static void main(String args[]) {
 		try {
 			IvyConfigListener listen = new IvyConfigListener(5);
+			listen.sendRequest();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
