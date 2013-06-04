5e1bda1ed3471c956495973fc42b7489e28ff5ab
florent <florent@debian>
2013-06-04 15:11:20 +0200

    Exception pour IvyConfigListener

---
+++ calibration/src/rawmode/GetConfigException.java
@@ -0,0 +1,18 @@
+package rawmode;
+
+public class GetConfigException extends Exception {
+
+	public GetConfigException() {
+		super();
+	}
+	
+	public GetConfigException(String s, Throwable e) {
+		super(s,e);
+	}
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
