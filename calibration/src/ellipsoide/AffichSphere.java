48bd4c7da6e67be31277841bb159576d36e06851
Alinoé <Alino@PC-de-Alinoe>
2013-06-04 10:49:41 +0200

    Intégration de sphere à testImu pour testaffichage

--- calibration/src/ellipsoide/AffichSphere.java
+++ calibration/src/ellipsoide/AffichSphere.java
@@ -1,37 +1,50 @@
 package ellipsoide;
 
-import java.util.ArrayList;
+import java.awt.Graphics;
+import java.awt.geom.Point2D;
 import java.util.List;
 
 import javax.swing.JPanel;
 
 public class AffichSphere extends JPanel {
-	
+
 	private Sphere s;
 	private List<Zone> zones;
-	
+
 	public void majZone() {
 		zones = s.getZones();
+		this.repaint();
 	}
-	
+
 	public void affiche() {
 		this.repaint();
 	}
-	
-	public void paintComponent(){
-		for(Zone z : zones){
-			z.getCoord();
+
+	public void paintComponent(Graphics g) {
+		List<Point2D> points;
+		int n = zones.get(1).getListContour().size();
+		int xPoints[] = new int[n];
+		int yPoints[] = new int[n];
+		int i = 0;
+		for (Zone z : zones) {
+			i = 0;
+			points = z.getListContour();
+			for (Point2D p : points) {
+				xPoints[i] = (int) p.getX();
+				yPoints[i] = (int) p.getY();
+				i++;
+			}
+			g.drawPolygon(xPoints, yPoints, n);
 		}
 	}
 
-	public AffichSphere(Sphere s){
+	public AffichSphere(Sphere s) {
 		this.s = s;
 	}
+
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
 
 }
--- calibration/src/imu/TestIMU.java
+++ calibration/src/imu/TestIMU.java
@@ -4,26 +4,18 @@ import testData.Sender;
 import common.TypeCalibration;
 import data.Data;
 import ellipsoide.Sphere;
-
-
-import filtre.FilterPlot;
+import filtre.FilterSphere;
 import filtre.GUIHelper;
-import filtre.Plotter;
 import fr.dgac.ivy.IvyException;
 
 public class TestIMU {
 	public static void main(String args[]) throws IvyException, InterruptedException {
 		TypeCalibration t = TypeCalibration.MAGNETOMETER;
 		Sphere sp = new Sphere(20,10);
-		Plotter plot = new Plotter(sp);
-		FilterPlot filtre = new FilterPlot(plot,10,TypeCalibration.MAGNETOMETER);
+		FilterSphere filtre = new FilterSphere(sp,10,TypeCalibration.MAGNETOMETER);
 		Data data = new Data(t,filtre);
-		GUIHelper.showOnFrame(plot, "test");
-
-		//Sender s = new Sender("C:\\Users\\Alino�\\Desktop\\13_05_29__10_15_23.data");
+		GUIHelper.showOnFrame(sp.getAffichage(), "test");
 		Sender s = new Sender("/home/paparazzi/var/logs/13_05_29__10_15_23.data");
-
-
 		IMU imu =new IMU(t,17,data);
 		s.start();
 		s.join();
