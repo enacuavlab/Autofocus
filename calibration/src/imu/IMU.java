d047e2dc81cef1c5635d5271fa0b4b0c7f658c8f
florent <florent@debian>
2013-06-10 15:02:20 +0200

    maj IMU pour verifier la connection du drone sur le bus

--- calibration/src/imu/IMU.java
+++ calibration/src/imu/IMU.java
@@ -3,8 +3,13 @@
  */
 package imu;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
+
+import javax.swing.JLabel;
+import javax.swing.Timer;
 
 import calibrate.PrintLog;
 
@@ -74,13 +79,18 @@ public class IMU implements IvyMessageListener {
 	 * log file
 	 */
 	private PrintLog log;
-
+	/**
+	 * label that checks the connection of the drone during the calibration
+	 */
+	private JLabel label;
+	
 	/**
 	 * allows to get back the right RAW_DATA messages
 	 * 
 	 * @throws IvyException
 	 */
 	public IMU() throws IvyException {
+		this.label = null;
 		this.log = null;
 		idDrone = -1;
 		// this.data=data;
@@ -109,11 +119,18 @@ public class IMU implements IvyMessageListener {
 	 * @param data
 	 * @param calibration
 	 */
-	public void ListenIMU(final Data data, final TypeCalibration calibration,final PrintLog log) {
+	public void ListenIMU(final Data data, final TypeCalibration calibration,final PrintLog log,final JLabel label) {
 		try {
+			this.label=label;
 			this.log=log;
 			this.data = data;
 			this.calibration = calibration;
+			final Timer timer = new Timer(1000,new ActionListener(){
+				public void actionPerformed(ActionEvent arg0) {
+					label.setText("false");
+				}
+			});
+			
 			// build the regexp according to parameters
 			StringBuffer regexp = new StringBuffer("^");
 			regexp.append(idDrone);
@@ -134,6 +151,8 @@ public class IMU implements IvyMessageListener {
 											.equals(calibration) ? " IMU_MAG_RAW"
 											: " IMU_ACCEL_RAW") + " " + args[0]
 									+ " " + args[1] + " " + args[2]);
+							timer.restart();
+							label.setText("true");
 				}
 			});
 
