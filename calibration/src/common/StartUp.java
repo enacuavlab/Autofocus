f129a284179d8563cf45c930d43e36b54d9359ff
gui <gui@Gui-PCport.lan>
2013-06-11 15:51:35 +0200

    Pour le jar

--- calibration/src/common/StartUp.java
+++ calibration/src/common/StartUp.java
@@ -43,11 +43,11 @@ public class StartUp {
 					panelDessin.validate();
 				}
 			});
- 			imu.ListenIMU(data, t, prlog);//Comment for test
+ 			//imu.ListenIMU(data, t, prlog);//Comment for test
 			
 			
 			//For test
-			/*Sender s;
+			Sender s;
 			try {
 				s = new Sender(System.getenv("HOME") +
 						"/paparazzi/var/logs/13_05_29__10_15_23.data");
@@ -63,7 +63,7 @@ public class StartUp {
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}*/
+			}
 			
 
 	}
@@ -89,9 +89,9 @@ public class StartUp {
 				panel.validate();
 			}
 		});
-		imu.ListenIMU(data, t, prlog); //Comment for test
+		//imu.ListenIMU(data, t, prlog); //Comment for test
 		// For test
-		/*Sender s;
+		Sender s;
 		try {
 			s = new Sender(System.getenv("HOME") + 
 					"/paparazzi/var/logs/13_04_03__13_49_35.data");
@@ -107,7 +107,7 @@ public class StartUp {
 		} catch (InterruptedException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
 		
 		
 		
