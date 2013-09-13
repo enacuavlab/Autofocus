7f55cf5adf80d7ddaa4e217a15f2e0e645d0e35c
alinoe <alinoe.abrassart@recherche.enac.fr>
2013-09-13 16:33:13 +0200

    interface imu manquante

---
+++ calibration/src/imu/IMUListener.java
@@ -0,0 +1,12 @@
+package imu;
+
+import java.util.EventListener;
+
+public interface IMUListener extends EventListener {
+	void aircraftModChanged(Aircraft ac) ;
+	void aircraftExited(Aircraft ac);
+	void aircraftConnected(Aircraft ac);
+	void aircraftRawOn(Aircraft ac);
+	void aircraftRawOff(Aircraft ac);
+	void aircraftCurrentExited(Aircraft ac);
+}
